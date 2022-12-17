50184069a14d22348f42bb6efc3ff6fec75b2f2a
Yogesh <bc21143@smsvaranasi.in>
2022-12-17 20:13:08 +0530

    first commit

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,3 +1,4 @@
+
 package com.driver;
 
 public class Car extends Vehicle {
@@ -9,17 +10,94 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
 
-    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
-        //Hint: Car extends Vehicle
-    }
 
-    public void changeGear(int newGear){
+    public int getWheels() {
+		return wheels;
+	}
+
+	public void setWheels(int wheels) {
+		this.wheels = wheels;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getDoors() {
+		return doors;
+	}
+
+	public void setDoors(int doors) {
+		this.doors = doors;
+	}
+
+	public int getGears() {
+		return gears;
+	}
+
+	public void setGears(int gears) {
+		this.gears = gears;
+	}
+
+	public boolean isManual() {
+		return isManual;
+	}
+
+	public void setManual(boolean isManual) {
+		this.isManual = isManual;
+	}
+
+	public int getCurrentGear() {
+		return currentGear;
+	}
+
+	public void setCurrentGear(int currentGear) {
+		this.currentGear = currentGear;
+	}
+
+	public int getSeats() {
+		return seats;
+	}
+
+	public void setSeats(int seats) {
+		this.seats = seats;
+	}
+
+	public Car(String name, int wheels, String type, int doors, int gears, boolean isManual, int currentGear,
+			int seats) {
+		super(name);
+		this.wheels = wheels;
+		this.type = type;
+		this.doors = doors;
+		this.gears = gears;
+		this.isManual = isManual;
+		this.currentGear = 1;
+		this.seats = seats;
+	}
+
+	public Car(String name, boolean isManual) {
+		super(name);
+		this.isManual = isManual;
+		
+	}
+
+	public void changeGear(int newGear){
+    	
+    	this.currentGear=newGear;
 
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
-    public void changeSpeed(int newSpeed, int newDirection){
 
+
+	public void changeSpeed(int newSpeed, int newDirection){
+    	
+    	 move(newSpeed,newDirection);
+          
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,11 +3,13 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
-        //Use arbitrary values for parameters which are not mentioned
+    	
+    	 super(name,isManual);
     }
 
+
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -17,14 +19,26 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-
-        if(newSpeed == 0) {
+        if(newSpeed==0) {
             //Stop the car, set gear as 1
+            stop();
+            setCurrentGear(1);
         }
         //for all other cases, change the gear accordingly
-
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+        else if(newSpeed > 0 && newSpeed<50) {
+            changeGear(1);
+        }else if(newSpeed>50 && newSpeed<100){
+            changeGear(2);
+        }else if(newSpeed>100 && newSpeed<150){
+            changeGear(3);
+        }else if(newSpeed>150 && newSpeed<200){
+            changeGear(4);
+        }else if(newSpeed>200 && newSpeed<250){
+            changeGear(5);
+        }else if(newSpeed>250){
+            changeGear(6);
+        }else{
+            changeGear(1);
         }
     }
 }
